ccb2e00bb158ea1b08d49c791e4a6a9eb26c396f
kumarpk12888 <kumarpk12888@gmail.com>
2026-07-08 10:31:58 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE = 'paperstack-v1';
+const SHELL = [
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      const fetchPromise = fetch(e.request).then(res => {
+        if(e.request.method === 'GET' && res.ok && e.request.url.startsWith(self.location.origin)){
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return res;
+      }).catch(() => cached);
+      return cached || fetchPromise;
+    })
+  );
+});
